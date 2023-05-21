5ecfe84b3cd2c97514a17d492ed70d4a8fa81964
Vitaliy Eremeev <evit.viter@gmail.com>
2023-05-22 03:44:47 +0700

    rename param == size

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -5,32 +5,32 @@ import pytest
 from selene import browser
 from selene.support.shared.jquery_style import s
 
-param_windows_desktop = [(1980, 1080),
-                         (1600, 1200),
-                         (1280, 960)]
+size_windows_desktop = [(1980, 1080),
+                        (1600, 1200),
+                        (1280, 960)]
 
-param_windows_mobile = [(428, 926),
-                        (372, 812),
-                        (414, 896)]
+size_windows_mobile = [(428, 926),
+                       (372, 812),
+                       (414, 896)]
 
 
-@pytest.fixture(params=param_windows_desktop)
-def param_windows_desktop(request):
+@pytest.fixture(params=size_windows_desktop)
+def size_windows_desktop(request):
     return request.param
 
-
-def test_github_desktop(param_windows_desktop):
+def test_github_desktop(size_windows_desktop):
     browser.open('')
-    browser.driver.set_window_size(param_windows_desktop[0], param_windows_desktop[1])
+    browser.driver.set_window_size(size_windows_desktop[0],
+                                   size_windows_desktop[1])
     s('.HeaderMenu-link--sign-in').click()
 
 
-@pytest.fixture(params=param_windows_mobile)
-def param_windows_mobile(request):
+@pytest.fixture(params=size_windows_mobile)
+def size_windows_mobile(request):
     return request.param
 
-
-def test_github_mobile(param_windows_mobile):
+def test_github_mobile(size_windows_mobile):
     browser.open('')
-    browser.driver.set_window_size(param_windows_mobile[0], param_windows_mobile[1])
+    browser.driver.set_window_size(size_windows_mobile[0],
+                                   size_windows_mobile[1])
     s('.flex-column [aria-label="Toggle navigation"]').click()
