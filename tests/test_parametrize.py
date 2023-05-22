8a7497412071c77d49499fd264ab0da9d96372de
Vitaliy Eremeev <evit.viter@gmail.com>
2023-05-22 18:29:31 +0700

    test parametrize passed passed

--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -6,33 +6,28 @@ from selene import browser
 from selene.support.shared.jquery_style import s
 
 
-@pytest.mark.parametrize("permision_w", [
-    (1920, 1080),
-    (1600, 900),
-    (1600, 1024),
-    (1280, 720),
-], ids=["1920, 108",
-        "1600, 900",
-        "1600, 1024",
-        "1280, 720"])
-def test_github_desktop(permision_w):
-    browser.driver.set_window_size(permision_w[0], permision_w[1])
-    browser.open('https://github.com/')
+@pytest.fixture(scope="function")
+def size_browser(request):
+    resolutions = {
+        "desktop": (1920, 1080),
+        "mobile": (428, 926)
+    }
+    return resolutions[request.param]
+
+
+@pytest.mark.parametrize("size_browser", ["desktop"], indirect=True)
+def test_desktop(size_browser):
+    browser.driver.set_window_size(*size_browser)
     s('.HeaderMenu-link--sign-in').click()
 
 
-@pytest.mark.parametrize("permision_m", [
-    (428, 926),
-    (372, 812),
-    (414, 896),
-    (414, 736),
-], ids=["428, 926",
-        "372, 812",
-        "414, 896",
-        "414, 736"])
-def test_github_mobile(permision_m):
-    browser.driver.set_window_size(permision_m[0], permision_m[1])
-    browser.open('https://github.com/')
+@pytest.mark.parametrize("size_browser", ["mobile"], indirect=True)
+def test_mobile(size_browser):
+    browser.driver.set_window_size(*size_browser)
     s('.flex-column [aria-label="Toggle navigation"]').click()
+
+
+
+
 
 
